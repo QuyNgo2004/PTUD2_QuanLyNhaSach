901a96c45d3e407eaf8b2040c3308088a006646a
NguyenAnhKiet <22211tt4497@mail.tdc.edu.vn>
2024-10-16 10:36:02 +0700

    Show Nha Phan Phoi va Chi Nhanh

---
+++ QuanLyHeThongNhaSach/system/BUS/BUS_ChiNhanh.cs
@@ -0,0 +1,32 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BUS
+{
+    public class BUS_ChiNhanh
+    {
+        private static BUS_ChiNhanh instance;
+
+        public static BUS_ChiNhanh Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new BUS_ChiNhanh();
+                }
+                return instance;
+            }
+        }
+
+        public void hienThiChiNhanh(DataGridView dgv)
+        {
+            dgv.DataSource = DAL_ChiNhanh.Instance.layDSChiNhanh();
+        }
+    }
+}
---
+++ QuanLyHeThongNhaSach/system/DAL/DAL_NhaPhanPhoi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DAL_NhaPhanPhoi
+    {
+        private static DAL_NhaPhanPhoi instance;
+
+        public static DAL_NhaPhanPhoi Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DAL_NhaPhanPhoi();
+                }
+                return instance;
+
+            }
+        }
+        QLNhaSachDataContext db = new QLNhaSachDataContext();
+
+        public IQueryable layDSNhaPhanPhoi()
+        {
+            IQueryable dsNhaCungCap = from ncc in db.NhaPhanPhois
+                                      select new 
+                                      {
+                                          MaNPP = ncc.maNPP,
+                                          TenNPP = ncc.tenNPP,
+                                          DiaChi = ncc.diachiNPP,
+                                          SoDienThoai = ncc.sdtNPP,
+                                          Email = ncc.emailNPP,
+                                      };
+            return dsNhaCungCap;
+        }
+    }
+}
