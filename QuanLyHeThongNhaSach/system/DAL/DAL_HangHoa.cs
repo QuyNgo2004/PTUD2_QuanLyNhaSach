a327f8b34564756f95bafa25ce5841f01eec1411
QuyNgo2004 <quyngocompurter2601@gmail.com>
2024-10-25 04:17:38 +0700

    Hang hoa

---
+++ QuanLyHeThongNhaSach/system/DAL/DAL_HangHoa.cs
@@ -0,0 +1,176 @@
+﻿using ET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DAL_HangHoa
+    {
+        //Khai báo biến tĩnh.
+        private static DAL_HangHoa instance;
+
+        public static DAL_HangHoa Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DAL_HangHoa();
+                }
+                return instance;
+            }
+        }
+
+        //Tạo một đối tượng 'db' từ lớp QLNhaSachDataContext.
+        private QLNhaSachDataContext db = new QLNhaSachDataContext();
+
+        //Xem danh sách loại hàng.
+        public IQueryable XemDSHangHoa()
+        {
+            IQueryable hanghoa = from hh in db.HangHoas
+                                 join hanghoa1 in db.LoaiHangHoas on hh.maLHH equals hanghoa1.maLHH
+                                 join hanghoa2 in db.NhaPhanPhois on hh.maNPP equals hanghoa2.maNPP
+                                 select new
+                                 {
+                                     Ma = hh.maHH,
+                                     Ten = hh.tenHH,
+                                     Loai = hanghoa1.tenLHH,
+                                     Gia = hh.giaHH,
+                                     DonViTinh = hh.donViTinh,
+                                     SoLuongTon = hh.soLuongTon,
+                                     NhaPhanPhoi = hanghoa2.tenNPP,
+                                     MoTa = hh.moTa,
+                                     GhiChu = hh.ghiChu,
+                                     TinhTrang = hh.tinhTrang
+                                     
+                                     
+
+                                 };
+            return hanghoa;
+        }
+
+        /// <summary>
+        /// Thêm hàng hóa.
+        /// </summary>
+        /// <param name="etKhachHang"></param>
+        /// <returns></returns>
+        public bool ThemHangHoa(ET_HangHoa etHH)
+        {
+            //Kiểm tra xem có trùng mã loại hàng hay không, nếu trùng trả về false.
+            if (db.HangHoas.Any(hh => hh.maHH == etHH.MaHH))
+            {
+                //Nếu tồn tại, trả về false để báo hiệu việc thêm không thành công do trùng lặp.
+                return false;
+            }
+            else
+            {
+                try
+                {
+                    //Tạo một đối tượng mới của loại hàng hóa.
+                    HangHoa hh = new HangHoa
+                    {
+                        maHH = etHH.MaHH,
+                        tenHH = etHH.TenHH,
+                        maLHH = etHH.LoaiHH,
+                        giaHH = etHH.GiaHH,
+                        donViTinh = etHH.DonviTinh,
+                        soLuongTon = etHH.SlTon,
+                        maNPP = etHH.NhaPP,
+                        moTa = etHH.MoTa,
+                        ghiChu = etHH.GhiChu,
+                        tinhTrang = etHH.TinhTrang,
+
+                    };
+                    //Thêm loại hàng vào cơ sở dữ liệu
+                    db.HangHoas.InsertOnSubmit(hh);
+                }
+                finally
+                {
+                    // Lưu các thay đổi vào cơ sở dữ liệu
+                    db.SubmitChanges();
+                }
+                // Trả về true để báo hiệu việc thêm mới thành công
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Xóa hàng hoa
+        /// </summary>
+        /// <returns></returns>
+        public bool XoaHangHoa(string maHH)
+        {
+            try
+            {
+                //Truy vấn lấy tất cả các bản ghi trong HangHoa có maLHH bằng với maHangHoa.
+                var xoa = from hh in db.HangHoas
+                          where hh.maLHH == maHH
+                          select hh;
+                // Duyệt qua từng bản ghi và xóa chúng khỏi cơ sở dữ liệu.
+                foreach (var x in xoa)
+                {
+                    db.HangHoas.DeleteOnSubmit(x);
+                    db.SubmitChanges();
+                }
+                // Nếu xóa thành công, trả về true.
+                return true;
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                // Xử lý các trường hợp ngoại lệ cụ thể, chẳng hạn liên quan đến ràng buộc khóa ngoại
+                if (ex.Number == 547)
+                {
+                    // Trả về false nếu có lỗi do ràng buộc khóa ngoại
+                    return false;
+                }
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Sửa hàng hóa.
+        /// </summary>
+        public void SuaHangHoa(ET_HangHoa etHH)
+        {
+            // Tìm đối tượng LoaiHangHoas trong cơ sở dữ liệu dựa trên maKH.
+
+            var update = db.HangHoas.Single(hh => hh.maLHH == etHH.MaHH);
+
+            // Cập nhật dựa trên thông tin nhận được.
+
+            update.maHH = etHH.MaHH;
+            update.tenHH = etHH.TenHH;
+            update.maLHH = etHH.LoaiHH;
+            update.giaHH = etHH.GiaHH;
+            update.donViTinh = etHH.DonviTinh;
+            update.soLuongTon = etHH.SlTon;
+            update.maNPP = etHH.NhaPP;
+            update.moTa = etHH.MoTa;
+            update.ghiChu = etHH.GhiChu;
+            update.tinhTrang = etHH.TinhTrang;
+
+            // Lưu các thay đổi vào cơ sở dữ liệu.
+            db.SubmitChanges();
+        }
+
+        //Tạo mã hàng hóa tự động.
+        public string TaoMaHangHoaTuDong()
+        {
+            //Đếm số lượng loại hàng.
+            int countMaHH = db.HangHoas.Count() + 1;
+
+            //Tạo mã mới.
+            string NewMaHH;
+            do
+            {
+                NewMaHH = $"KH{countMaHH}";
+                countMaHH++;
+            } while (db.HangHoas.Any(kh => kh.maHH == NewMaHH));
+            return NewMaHH;
+        }
+    }
+}
--- QuanLyHeThongNhaSach/system/GUI/HangHoa.cs
+++ QuanLyHeThongNhaSach/system/GUI/HangHoa.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BUS;
+using ET;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,16 +14,90 @@ namespace GUI
 {
     public partial class HangHoa : Form
     {
+        private BUS_LoaiHangHoa lhh = BUS_LoaiHangHoa.Instance;
+        private BUS_NhaPhanPhoi npp = BUS_NhaPhanPhoi.Instance;
         public HangHoa()
         {
             InitializeComponent();
         }
 
+        private Menu mainForm;
+
+        public HangHoa(Menu mainForm)
+        {
+            InitializeComponent();
+            this.mainForm = mainForm;
+        }
+
+        private void HangHoa_Load(object sender, EventArgs e)
+        {
+            BUS_HangHoa.Instance.XemDSHH(dgvDSHangHoa);
+            txtMaHH.Text = BUS_HangHoa.Instance.TaoMaHangTuDong();
+
+            try
+            {
+
+                cboLoaiHH.DataSource = lhh.LoadDSloaiHH();
+                cboNhaPP.DataSource = npp.loadnpp();
+                cboLoaiHH.DisplayMember = "tenLHH";
+                cboLoaiHH.ValueMember = "maLHH";
+                cboNhaPP.ValueMember = "maNPP";
+                cboNhaPP.DisplayMember = "tenNPP";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi load form: " + ex.Message, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        private void dgvDSHangHoa_Click(object sender, EventArgs e)
+        {
+            int a = dgvDSHangHoa.CurrentCell.RowIndex;
+
+            txtMaHH.Text = dgvDSHangHoa.Rows[a].Cells[0].Value?.ToString() ?? "";
+            txtTenHH.Text = dgvDSHangHoa.Rows[a].Cells[1].Value?.ToString() ?? "";
+            cboLoaiHH.Text = dgvDSHangHoa.Rows[a].Cells[2].Value?.ToString() ?? "";
+            txtGiaHH.Text = dgvDSHangHoa.Rows[a].Cells[3].Value?.ToString() ?? "";
+            txtDonViTinh.Text = dgvDSHangHoa.Rows[a].Cells[4].Value?.ToString() ?? "";
+            txtSoLuongTon.Text = dgvDSHangHoa.Rows[a].Cells[5].Value?.ToString() ?? "";
+            cboNhaPP.Text = dgvDSHangHoa.Rows[a].Cells[6].Value?.ToString() ?? "";
+            txtMoTa.Text = dgvDSHangHoa.Rows[a].Cells[7].Value?.ToString() ?? "";
+            txtGhiChu.Text = dgvDSHangHoa.Rows[a].Cells[8].Value?.ToString() ?? "";
+            cboTinhTrang.Text = dgvDSHangHoa.Rows[a].Cells[9].Value?.ToString() ?? "";
+        }
+
+        public bool KTraMa(string maLHH)
+        {
+            if (maLHH.Equals(dgvDSHangHoa.CurrentRow.Cells[0].Value.ToString()) == true)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool KtraBoTrong()
+        {
+            if (txtMaHH.Text.Length == 0 || txtTenHH.Text.Length == 0 || txtGiaHH.Text.Length == 0 || txtDonViTinh.Text.Length == 0 || txtSoLuongTon.Text.Length == 0 || txtMoTa.Text.Length == 0 || txtGhiChu.Text.Length == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
         /// <summary>
-        /// button thoát.
+        /// Thanh công cụ.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
+
+        private void btnThemLHH_Click(object sender, EventArgs e)
+        {
+            //Menu formMenu = (Menu)this.ParentForm;
+            //formMenu.openChildForm(new LoaiHangHoa());
+
+            LoaiHangHoa lhh = new LoaiHangHoa();
+            lhh.Show();
+        }
 
         private void btnThoat_Click(object sender, EventArgs e)
         {
@@ -32,16 +108,76 @@ namespace GUI
             }
         }
 
-        /// <summary>
-        /// button trở lại nhập hàng.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-
         private void btnTroLaiNhapHang_Click(object sender, EventArgs e)
         {
             Menu formMenu = (Menu)this.ParentForm;
             formMenu.openChildForm(new NhapHang());
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            DialogResult d = MessageBox.Show("Xác nhận thêm dữ liệu đã nhập ?", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (d == DialogResult.Yes)
+            {
+                if (KtraBoTrong() == true)
+                {
+                    BUS_HangHoa.Instance.ThemHangHoa(new ET_HangHoa(txtMaHH.Text, txtTenHH.Text, cboLoaiHH.SelectedValue.ToString(), int.Parse(txtGiaHH.Text), txtDonViTinh.Text, int.Parse(txtSoLuongTon.Text), cboNhaPP.SelectedValue.ToString(), txtMoTa.Text, txtGhiChu.Text, cboTinhTrang.Text));
+                    BUS_HangHoa.Instance.XemDSHH(dgvDSHangHoa);
+                }
+                else
+                {
+                    MessageBox.Show("Không thể bỏ trống bất kì trường nào !", "THÔNG BÁO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (dgvDSHangHoa.SelectedRows.Count > 0)
+            {
+                DialogResult d = MessageBox.Show("Hãy chắc chắn rằng bạn muốn xóa dữ liệu vừa nhập !", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (d == DialogResult.Yes)
+                {
+                    BUS_HangHoa.Instance.XoaHangHoa(dgvDSHangHoa);
+                    BUS_HangHoa.Instance.XemDSHH(dgvDSHangHoa);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn dòng", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (dgvDSHangHoa.SelectedRows.Count > 0)
+            {
+                DialogResult d = MessageBox.Show("Hãy chắc chắn rằng bạn muốn sửa dữ liệu vừa nhập !", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (d == DialogResult.Yes)
+                {
+                    if (KtraBoTrong() == true)
+                    {
+                        if (KTraMa(txtMaHH.Text) == true)
+                        {
+                            BUS_HangHoa.Instance.SuaHangHoa(new ET_HangHoa(txtMaHH.Text, txtTenHH.Text, cboLoaiHH.Text, int.Parse(txtGiaHH.Text), txtDonViTinh.Text, int.Parse(txtSoLuongTon.Text), cboNhaPP.Text, txtMoTa.Text, txtGhiChu.Text, cboTinhTrang.Text));
+                            BUS_HangHoa.Instance.XemDSHH(dgvDSHangHoa);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không thể sửa mã loại hàng !", "THÔNG BÁO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            txtMaHH.Text = dgvDSHangHoa.CurrentRow.Cells[0].Value.ToString();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không thể bỏ trống bất kì trường nào !", "THÔNG BÁO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn dòng", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
